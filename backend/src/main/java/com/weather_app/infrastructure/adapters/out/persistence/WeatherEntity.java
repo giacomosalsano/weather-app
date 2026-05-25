4b0a5545c6b10edd86a63e4470bed9efc75de97a
Giacomo Salsano <giacomosalsano@hotmail.com>
2026-05-25 18:31:43 +0100

    feat: implemented WeatherEntity

---
+++ backend/src/main/java/com/weather_app/infrastructure/adapters/out/persistence/WeatherEntity.java
@@ -0,0 +1,34 @@
+package com.weather_app.infrastructure.adapters.out.persistence;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_weather")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WeatherEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String city;
+
+    private Double temperature;
+    private String description;
+    private LocalDateTime timestamp;
+}
